7fefa9419a7f6ebbcd9fc2461e44b9bd13ea459b
ronah289 <ronald.kibet@student.moringaschool.com>
2021-10-04 15:33:45 -0230

    move goals array: appcompnent to goalcomponent

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Goal} from '../goal';
 
 @Component({
   selector: 'app-goal',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./goal.component.css']
 })
 export class GoalComponent implements OnInit {
+  goals:Goal[] = [
+    {id:1, name:'Watch finding Nemo'},
+    {id:2,name:'Buy Cookies'},
+    {id:3,name:'Get new Phone Case'},
+    {id:4,name:'Get Dog Food'},
+    {id:5,name:'Solve math homework'},
+    {id:6,name:'Plot my world domination plan'},
+  ];
 
   constructor() { }
 
