2eb133415ad40acecde64055a0b5737f52995db5
ronah289 <ronald.kibet@student.moringaschool.com>
2021-10-04 15:27:22 -0230

    display array items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Goal} from './goal';
 
 @Component({
   selector: 'app-root',
@@ -7,8 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Goals'
-  goals!:string[];
-  constructor(){
-    this.goals = ['buy watch','buy phone','buy shoes','attend meeting'];
-  }
+  goals:Goal[] = [
+    {id:1, name:'Watch finding Nemo'},
+    {id:2,name:'Buy Cookies'},
+    {id:3,name:'Get new Phone Case'},
+    {id:4,name:'Get Dog Food'},
+    {id:5,name:'Solve math homework'},
+    {id:6,name:'Plot my world domination plan'},
+  ];
 }
