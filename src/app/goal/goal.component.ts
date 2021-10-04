186d41e02f35f8cc82dd96d5076e11aa96f43025
ronah289 <ronald.kibet@student.moringaschool.com>
2021-10-04 15:41:21 -0230

    add description to goals

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -7,15 +7,14 @@ import {Goal} from '../goal';
   styleUrls: ['./goal.component.css']
 })
 export class GoalComponent implements OnInit {
-  goals:Goal[] = [
-    {id:1, name:'Watch finding Nemo'},
-    {id:2,name:'Buy Cookies'},
-    {id:3,name:'Get new Phone Case'},
-    {id:4,name:'Get Dog Food'},
-    {id:5,name:'Solve math homework'},
-    {id:6,name:'Plot my world domination plan'},
+  goals: Goal[] = [
+    {id:1, name:'Watch finding Nemo',description:'Find an online version and watch merlin find his son'},
+    {id:2,name:'Buy Cookies',description:'I have to buy cookies for the parrot'},
+    {id:3,name:'Get new Phone Case',description:'Diana has her birthday coming up soon'},
+    {id:4,name:'Get Dog Food',description:'Pupper likes expensive sancks'},
+    {id:5,name:'Solve math homework',description:'Damn Math'},
+    {id:6,name:'Plot my world domination plan',description:'Cause I am an evil overlord'},
   ];
-
   constructor() { }
 
   ngOnInit(): void {
