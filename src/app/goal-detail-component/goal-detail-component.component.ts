8a3f8860493fff5505f081d13b4ebff889e01aae
ronah289 <ronald.kibet@student.moringaschool.com>
2021-10-04 16:07:31 -0230

    add input property binding

--- src/app/goal-detail-component/goal-detail-component.component.ts
+++ src/app/goal-detail-component/goal-detail-component.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
+import { Goal } from '../goal';
 
 @Component({
-  selector: 'app-goal-detail-component',
+  selector: 'app-goal-detail',
   templateUrl: './goal-detail-component.component.html',
   styleUrls: ['./goal-detail-component.component.css']
 })
 export class GoalDetailComponentComponent implements OnInit {
 
+  @Input() goal!:Goal;
   constructor() { }
 
   ngOnInit(): void {
