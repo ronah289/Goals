18ec85ca87b03b1c9adad68d0756bea82bb85faa
ronah289 <ronald.kibet@student.moringaschool.com>
2021-10-06 19:58:03 -0230

    add form submission

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -15,6 +15,12 @@ export class GoalComponent implements OnInit {
     new Goal(5, 'Solve math homework', 'Damn Math', new Date(2019, 2, 14)),
     new Goal(6, 'Plot my world domination plan', 'Cause I am an evil overlord', new Date(2030, 3, 14)),
   ];
+  addNewGoal(goal:any){
+    let goalLength = this.goals.length;
+    goal.id = goalLength+1;
+    goal.completeDate = new Date(goal.completeDate)
+    this.goals.push(goal)
+  }
 
   toggleDetails(index: any) {
     this.goals[index].showDescription = !this.goals[index].showDescription;
