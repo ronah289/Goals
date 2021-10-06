ff148d0f19eb7467d1561c5114d1653c117e20f6
ronah289 <ronald.kibet@student.moringaschool.com>
2021-10-06 13:15:39 -0230

    delete goal

--- src/app/goal-detail-component/goal-detail-component.component.ts
+++ src/app/goal-detail-component/goal-detail-component.component.ts
@@ -9,6 +9,7 @@ import { Goal } from '../goal';
 export class GoalDetailComponentComponent implements OnInit {
 
   @Input() goal!: Goal;
+  
   @Output() isComplete = new EventEmitter<boolean>();
   goalComplete(complete: boolean) {
     this.isComplete.emit(complete);
